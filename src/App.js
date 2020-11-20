d3f162c33f06d096375d4e1da4758cc73fe8416f
Kebur Lulseged <kebur.lulseged@gmail.com>
2020-11-20 09:40:12 -0800

    Set up DirectoryComponent

--- src/App.js
+++ src/App.js
@@ -2,17 +2,28 @@ import logo from './logo.svg';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import React, { Component } from 'react';
+import Directory from './components/DirectoryComponent';
+import { CAMPSITES } from './shared/campsites'
 
-function App() {
-  return (
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      campsites: CAMPSITES
+    }
+  }
+  render() {
+    return (
     <div className="App">
       <Navbar dark color="primary">
         <div className="container">
           <NavbarBrand href='/'>NuCamp</NavbarBrand>
         </div>
       </Navbar>
+      <Directory campsites={this.state.campsites}/>
     </div>
   );
+  }
 }
 
 export default App;
