3e72cf19b82479dc6bee01041bf5b4a3bb03808d
Kebur Lulseged <kebur.lulseged@gmail.com>
2020-11-24 14:25:10 -0800

    Add main compnent, and refactor directory and app component

---
+++ src/components/MainComponent.js
@@ -0,0 +1,38 @@
+import { Navbar, NavbarBrand } from 'reactstrap';
+import React, { Component } from 'react';
+import Directory from './DirectoryComponent';
+import { CAMPSITES } from '../shared/campsites'
+import CampsiteInfo from './CampsiteInfoComponent'
+
+
+class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      campsites: CAMPSITES,
+      selectedCampsite: null
+    }
+  }
+  onCampsiteSelect(campsiteId) {
+    this.setState({selectedCampsite: campsiteId})
+  }
+
+  render() {
+    console.log(this.state.campsites);
+    return (
+    <div className="App">
+      <Navbar dark color="primary">
+        <div className="container">
+          <NavbarBrand href='/'>NuCamp</NavbarBrand>
+        </div>
+      </Navbar>
+      <Directory
+        campsites={this.state.campsites}
+        onClick={campsiteId => this.onCampsiteSelect(campsiteId)}/>
+      <CampsiteInfo campsite={this.state.campsites.filter(campsite => campsite.id === this.state.selectedCampsite)[0]}/>
+    </div>
+  );
+  }
+}
+
+export default Main;
