efd8132a3c1e86df7e4f6c88c20e442ac4f7895a
Kebur Lulseged <kebur.lulseged@gmail.com>
2020-11-27 13:19:04 -0800

    Render featured items on home page

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import Directory from './DirectoryComponent';
-import { CAMPSITES } from '../shared/campsites'
+import { CAMPSITES } from '../shared/campsites';
+import { COMMENTS } from '../shared/comments';
+import { PARTNERS } from '../shared/partners';
+import { PROMOTIONS } from '../shared/promotions';
 import CampsiteInfo from './CampsiteInfoComponent'
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
+import Contact from './ContactComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 
@@ -13,12 +17,19 @@ class Main extends Component {
     super(props);
     this.state = {
       campsites: CAMPSITES,
+      comments: COMMENTS,
+      partners: PARTNERS,
+      promotions: PROMOTIONS
     }
   }
   render() {
     const HomePage = () => {
       return (
-        <Home />
+        <Home
+          campsite={this.state.campsites.filter(campsite => campsite.featured)[0]}
+          promotion={this.state.promotions.filter(promotion => promotion.featured)[0]}
+          partner={this.state.partners.filter(partner => partner.featured)[0]}
+        />
       )
     }
 
@@ -28,6 +39,7 @@ class Main extends Component {
       <Switch>
         <Route path='/home' component={HomePage} />
         <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites}/>} />
+        <Route exact path='/contactus' component={Contact} />
         <Redirect to='/home' />
       </Switch>
       <Footer />
