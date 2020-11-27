a7ae02f36633b869581663e7fe3ad1b2938f6e99
Kebur Lulseged <kebur.lulseged@gmail.com>
2020-11-27 12:13:40 -0800

    Add Header and Footer components

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,8 +1,9 @@
-import { Navbar, NavbarBrand } from 'reactstrap';
 import React, { Component } from 'react';
 import Directory from './DirectoryComponent';
 import { CAMPSITES } from '../shared/campsites'
 import CampsiteInfo from './CampsiteInfoComponent'
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
 
 
 class Main extends Component {
@@ -21,15 +22,12 @@ class Main extends Component {
 
     return (
     <div className="App">
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href='/'>NuCamp</NavbarBrand>
-        </div>
-      </Navbar>
+      <Header />
       <Directory
         campsites={this.state.campsites}
         onClick={campsiteId => this.onCampsiteSelect(campsiteId)}/>
       <CampsiteInfo campsite={this.state.campsites.filter(campsite => campsite.id === this.state.selectedCampsite)[0]}/>
+      <Footer />
     </div>
   );
   }
