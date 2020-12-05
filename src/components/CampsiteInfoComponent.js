c266d517a7db6f5852136be6063e81fbcad85700
Kebur Lulseged <kebur.lulseged@gmail.com>
2020-12-05 11:43:17 -0800

    Add CommentFrom component with modal to submit comments

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import {Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import React, { Component } from 'react';
+import {Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader, ModalBody, Row, Col, Label } from 'reactstrap';
 import { Link } from 'react-router-dom';
-
+import { Control, LocalForm, Errors } from 'react-redux-form';
 
 
 
@@ -38,6 +38,7 @@ import { Link } from 'react-router-dom';
             )
             })
           }
+          <CommentForm />
         </div>
       )
     }
@@ -45,6 +46,77 @@ import { Link } from 'react-router-dom';
       <div></div>
     )
   }
+
+ class CommentForm extends Component {
+   constructor(props) {
+     super(props);
+     this.state = {
+       isModalOpen: false
+     }
+
+     this.toggleModal = this.toggleModal.bind(this);
+   }
+
+   toggleModal() {
+     this.setState({
+       isModalOpen: !this.state.isModalOpen
+     })
+   }
+
+   handleSubmit(values) {
+    alert("Current state is: " + JSON.stringify(values));
+   }
+
+
+   render() {
+     return (
+       <React.Fragment>
+         <Button onClick={this.toggleModal} outline>
+           <i className="fa fa-sign-in fa-lg" /> Sumbit Comment
+         </Button>
+
+         <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+           <ModalHeader>Submit Comment</ModalHeader>
+           <ModalBody>
+             <LocalForm onSubmit={values => this.handleSubmit(values)}>
+               <Row className="form-group">
+                 <Label htmlFor="rating" md={2}>Rating</Label>
+                 <Col md={10}>
+                   <Control.select model=".rating" id="rating" name="rating" className="form-control" defaultValue="5">
+                     <option value="1">1</option>
+                     <option value="2">2</option>
+                     <option value="3">3</option>
+                     <option value="4">4</option>
+                     <option value="5">5</option>
+                   </Control.select>
+                 </Col>
+               </Row>
+               <Row className="form-group">
+                 <Label htmlFor="author" md={2}>Your Name</Label>
+                 <Col md={10}>
+                   <Control.text model=".author" id="author" name="author" className="form-control" />
+                 </Col>
+               </Row>
+               <Row className="form-group">
+                 <Label htmlFor="comment" md={2}>Comment</Label>
+                 <Col md={10}>
+                   <Control.textarea model=".comment" rows="6" id="comment" name="comment" className="form-control" />
+                 </Col>
+               </Row>
+               <Row className="form-group">
+                 <Col md={{size: 10, offset: 2}}>
+                   <Button type="submit" color="primary">
+                     Submit
+                   </Button>
+                 </Col>
+               </Row>
+             </LocalForm>
+           </ModalBody>
+         </Modal>
+       </React.Fragment>
+     )
+   }
+    }
 
   const CampsiteInfo = (props) => {
     if(props.campsite) {
@@ -64,6 +136,7 @@ import { Link } from 'react-router-dom';
             <RenderCampsite campsite={props.campsite} />
             <RenderComments comments={props.comments} />
           </div>
+
         </div>
       )
     } else {
