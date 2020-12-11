b1dbf320739a8cfddcd3219d2bfafbb6f667caee
Kebur Lulseged <kebur.lulseged@gmail.com>
2020-12-10 18:16:10 -0800

    update to support loading and failed views when loading campsites data

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import {Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader, ModalBody, Row, Col, Label } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Loading } from './LoadingComponent';
+
 
 
 
@@ -169,6 +171,26 @@ import { Control, LocalForm, Errors } from 'react-redux-form';
     }
 
   const CampsiteInfo = (props) => {
+    if (props.isLoading) {
+      return (
+        <div className="container">
+          <div className="row">
+            <Loading />
+          </div>
+        </div>
+      )
+    }
+    if (props.errMess) {
+      return (
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <h4>{props.errMess}</h4>
+            </div>
+          </div>
+        </div>
+      )
+    }
     if(props.campsite) {
       return (
         <div className="container">
