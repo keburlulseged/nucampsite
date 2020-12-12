838907c0f58ad3e482e5785bdd3f9ebc26e8c607
Kebur Lulseged <kebur.lulseged@gmail.com>
2020-12-12 13:06:56 -0800

    add transitions

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent'
 import Header from './HeaderComponent';
@@ -66,14 +67,18 @@ class Main extends Component {
     return (
     <div className="App">
       <Header />
-      <Switch>
-        <Route path='/home' component={HomePage} />
-        <Route exact path='/directory' render={() => <Directory campsites={this.props.campsites}/>} />
-        <Route path='/directory/:campsiteId' component={CampsiteWithId} />
-        <Route exact path='/contactus' render={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />} />
-        <Route exact path='/aboutus' render={() => <About partners={this.props.partners} />} />
-        <Redirect to='/home' />
-      </Switch>
+      <TransitionGroup>
+        <CSSTransition key={this.props.location.key} className="page" timeout={300}>
+          <Switch>
+            <Route path='/home' component={HomePage} />
+            <Route exact path='/directory' render={() => <Directory campsites={this.props.campsites}/>} />
+            <Route path='/directory/:campsiteId' component={CampsiteWithId} />
+            <Route exact path='/contactus' render={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />} />
+            <Route exact path='/aboutus' render={() => <About partners={this.props.partners} />} />
+            <Redirect to='/home' />
+          </Switch>
+        </CSSTransition>
+      </TransitionGroup>
       <Footer />
     </div>
   );
